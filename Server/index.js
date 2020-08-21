20dbe595a81403b12e7e5ce2a517c967c1d7324c
Joseph Lee <17460257+josephl70@users.noreply.github.com>
2020-08-21 13:19:50 -0400

    Added the ability to load levels

--- Server/index.js
+++ Server/index.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 const ip = "localhost";
 const port = 3000;
 const GameName = "";
+const GameStringID = 0;
 
 function chunkArray(arr, size) {
   var myArray = [];
@@ -22,14 +23,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('gamecreator/thumbnails'));
 
-app.use('/authToken', function(req, res, next) {
+app.get('/crossdomain.xml', function(req, res, next) {
+  res.type('application/xml');
+  res.send('<?xml version="1.0"?><!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd"><cross-domain-policy></cross-domain-policy>');
+});
+
+app.post('/authToken', function(req, res, next) {
   res.send("authService.tokens.missing.authid Missing authid token authService.tokens.missing.pid Missing PID\nauthService.tokens.invalid.credentials Something isn't working right. Please try again later.");
+});
+
+app.get('/game-creator/' + GameName + '/random', function(req, res, next) {
+  glob(__dirname + '/gamecreator/json_levels/*.xml', {}, (err, files)=>{
+    fs.readFile(files[Math.floor(Math.random() * files.length)], function(err, data) {
+      res.type('application/json');
+      res.send(data);
+    });
+  });
+});
+
+app.get('/gamecreator/cache_storage/images//:xml', function(req, res, next) {
+  res.type('application/json');
+  res.send("[]");
+});
+app.get('/gamecreator/cache_storage/images2//:xml', function(req, res, next) {
+  res.type('application/json');
+  res.send("[]");
+});
+
+app.get('/game-creator/' + GameName + "/:gamename", function(req, res, next) {
+  fs.readFile("gamecreator/json_levels/" + req.params.gamename + ".xml", function(err, data) {
+    res.type('application/json');
+    res.send(data);
+  });
 });
 
 app.get('/game-creator-list/featured/' + GameName + '/:page', function(req, res, next) {
     var MaxLevelsPerPage = 10;
     glob(__dirname + '/gamecreator/json_levels/*.xml', {}, (err, files)=>{
-      console.log(files);
       if(files.length > 0)
       {
         let FeaturedArray = [];
@@ -52,28 +82,28 @@ app.get('/game-creator-list/featured/' + GameName + '/:page', function(req, res,
       }
       else
       {
-        res.set('Content-Type', 'application/json;charset=utf-8');
+        res.type('application/json');
         res.send("[]");
       }
     });
 });
 
-app.use('/game-creator/' + GameName + '/random', function(req, res, next) {
-  glob(__dirname + '/gamecreator/xml_levels/*.xml', {}, (err, files)=>{
-    fs.readFile(files[Math.floor(Math.random() * files.length)], function(err, data) {
-      res.set('Content-Type', 'application/json;charset=utf-8');
-      res.send(data);
-    });
-  });
-});
-
 app.post('/game-creator/' + GameName, function(req, res, next) {
   if(req.body["requestType"] === "saveGame")
   {
-      var NameArray = new Array("", "Adventure", "Alien", "Ambush", "Animo", "Assault", "Atomic", "Attack", "Battle", "Beast", "Ben", "Blaster", "Breaker", "Burst", "Challenge", "Champion", "Charge", "Chase", "Clash", "Cosmic", "Crusher", "Danger", "Dark", "Defense", "Doom", "Droid", "Drome", "Earth", "Energy",
-      "Escape", "Fire", "Force", "Fury", "Fusion", "Galaxy", "Gear", "Giga", "Hero", "Hex", "Humungousaur", "Hyper", "Invasion", "Iron", "Jetray", "Laser", "Light", "Lightning", "Magno", "Mega", "Metal", "Mission", "Nano", "Nitro", "Nova", "Patrol", "Plasma", "Plumber", "Power", "Pulse", "Pursuit", "Quake", "Racer", 
-      "Rampage", "Razor", "Rescue", "Robot", "Rock", "Rocket", "Rumble", "Rush", "Rustbucket", "Search", "Shadow", "Shock", "Siege", "Slam", "Smash", "Solar", "Sonic", "Space", "Spark", "Spidermonkey", "Spike", "Star", "Steel", "Stone", "Storm", "Streak", "Swampfire", "Target", "Techno", "Thunder", "Titan", "Turbo", "Ultimate", "Ultra", 
-      "Universe", "Vilgax", "Warp", "Warrior", "Brainstorm", "Terraspin", "Water", "Waterhazard", "Whirlwind", "Big", "Cannonbolt", "Chill", "Echo", "Omniverse", "Feedback", "Gravattack", "Ball Weevil", "Bloxx", "Rook", "Kickin Hawk", "Snare-Oh", "Blitzwolf");
+      var NameArray = [];
+      switch(GameStringID)
+      {
+        case 0:
+          NameArray = fs.readFileSync('gamecreator/BGC_Names.txt', 'utf8').split('\n');
+        break;
+        case 2:
+          NameArray = fs.readFileSync('gamecreator/AGC_Names.txt', 'utf8').split('\n');
+        break;
+        case 3:
+          NameArray = fs.readFileSync('gamecreator/BMGC_Names.txt', 'utf8').split('\n');
+        break;
+      }
       var UniqueIDString = req.body["goal-id"] + req.body["hero-id"] + req.body["game-name"];
       UniqueIDString.replace(",", "");
       var CryptoID = crypto.createHash('sha1').update(UniqueIDString).digest('hex');
@@ -83,31 +113,19 @@ app.post('/game-creator/' + GameName, function(req, res, next) {
       var PlayerName = req.body["player-name"];
       //if (PlayerName == ""){PlayerName = "Guest";} //ONLY USE THIS IF YOU ARE HANDLING ACCOUNT SYSTEMS
       fs.writeFile("gamecreator/thumbnails/" + PlayerName + "/" + UniqueID + ".jpg", data, 'base64', function(err) {
-        if(err == null)
-        {
-          console.log(err);
-        }
+          if(err) { console.log("Error: " + err); }
       });
       var GameNameArray = req.body["game-name"].split(",");
-      GameNameArray[0] += 0;
-      GameNameArray[1] += 0;
-      GameNameArray[2] += 0;
-      var GameTextName = NameArray[GameNameArray[0]] + " " + NameArray[GameNameArray[1]] + " " + NameArray[GameNameArray[2]];
+      var GameTextName = NameArray[Number(GameNameArray[0])].replace('\r', '') + " " + NameArray[Number(GameNameArray[1])].replace('\r', '') + " " + NameArray[Number(GameNameArray[2])].replace('\r', '');
       var XMLCacheData = '<?xml version="1.0"?>\n<root>\n<response status="SUCCESS"/>\n<game id="' + UniqueID + '" rating="0" difficulty="0" status="0" timeToBeat="0" createDate="07/01/2010">\n<thumbnail>http://' + ip + ":" + port + "/" + "gamecreator/xml_levels/" + PlayerName + "/" + UniqueID + ".xml" + '</thumbnail>\n<title>' + GameTextName + '</title>\n<creator>' + PlayerName + '</creator>\n<levelData>' + req.body["game-data"] + '</levelData>\n<relatedgames/>\n</game>\n</root>';
       var JsonData = '{"heroId":' + req.body["hero-id"] + ',"goalId":' + req.body["goal-id"] + ',"timeToBeat":0,"gameData":"' + req.body["game-data"] + '","id":"' + UniqueID + '","productName":"gamecreator","gameName":"' + GameTextName + '","playerName":"' + PlayerName + '","timesPlayed":0,"lastTimePlayed":1375410277138,"difficulty":0,"rating":0,"difficultyCount":0,"ratingCount":0,"ratingAverage":0,"featured":' + req.body["featured"] + ',"featuredDate":0,"timesCompleted":0,"flagCount":0,"thumbnailUrl":"http://' + ip + ":" + port + "/" + UniqueID + ".jpg" + '","thumbnailBase64Encoded":"' + req.body["encoded-thumbnail"] + '","approveDate":1375242495523,"rejectDate":0,"approveBy":"TurnOut_GC3","createdBy":"aa41859-1202333211-1375240985621-1","createdDate":1375242400748,"version":1,"versionUUID":"6567c0f0-13c6-4f2b-8cbf-a25918dbb3b9"}';;
       fs.writeFile("gamecreator/json_levels/" + UniqueID + "." + req.body["mode"], JsonData, function(err) {
-        if(err == null)
-        {
-          console.log(err);
-        }
+          if(err) { console.log("Error: " + err); }
       });
       fs.writeFile("gamecreator/xml_levels/" + PlayerName + "/" + UniqueID + ".xml", XMLCacheData, function(err) {
-        if(err == null)
-        {
-          console.log(err);
-        }
+          if(err) { console.log("Error: " + err); }
       });
-      res.header("Content-Type: application/json");
+      res.type('application/json');
       res.send(JsonData);
   }
   else
@@ -116,16 +134,9 @@ app.post('/game-creator/' + GameName, function(req, res, next) {
   }
 });
 
-/*app.use('/game-creator/' + GameName + "/:gamename", function(req, res, next) {
-  fs.readFile("gamecreator/json_levels/" + req.params.gamename, function(err, data) {
-    res.set('Content-Type', 'application/json;charset=utf-8');
-    res.send(data);
-  });
-});*/
-
-app.use('/', function(req, res, next) {
+/*app.use('/', function(req, res, next) {
     res.set('Content-Type', 'application/json;charset=utf-8');
     res.send("[]");
-});
+});*/
 
 app.listen(port, ip);
